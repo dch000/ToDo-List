e7597317f66c54b9b2a84c2262703f9a0a687fd4
dch000 <dmitriy.ev.ch@yandex.ru>
2023-05-09 19:18:51 +0300

    +

--- ToDo List/Model.swift
+++ ToDo List/Model.swift
@@ -1,29 +1,33 @@
 import Foundation
 
-var toDoItems: [[String: Any]] = [["Name": "Позвонить маме", "isCompleted" : true],["Name": "Заказать пиццу", "isCompleted" : false]]
+var toDoItems: [[String: Any]] {
+    set {
+        UserDefaults.standard.setValue(newValue, forKey: "ToDoDataKey")
+        UserDefaults.standard.synchronize()
+    }
+    get {
+        if let array = UserDefaults.standard.array(forKey: "ToDoDataKey") as? [[String: Any]] {
+            return array
+        } else {
+            return []
+        }
+    }
+}
 
 //Добавление
 func addItem(nameItem: String, isCompleted: Bool = false) {
     toDoItems.append(["Name" : nameItem, "isCompleted" : false])
-    saveData()
 }
 
 //Удаление
 func removeItem(at index: Int) {
     toDoItems.remove(at: index)
-    saveData()
+    
 }
-
-//Сохранение данных
-func saveData() {}
-
-//Загрузка данных
-func loadData() {}
 
 //Меняем состояние дела - Выполнено / Не выполнено
 //получаем значение и даем ему противоположное состояние
 func changeState(at item: Int) -> Bool {
     toDoItems[item]["isCompleted"] = !(toDoItems[item]["isCompleted"] as! Bool)
-    saveData()
     return toDoItems[item]["isCompleted"] as! Bool
 }
