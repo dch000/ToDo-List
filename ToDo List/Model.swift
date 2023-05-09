5c264b5a2a27e2f10615860d1a142a3f4b610f79
dch000 <dmitriy.ev.ch@yandex.ru>
2023-05-09 18:09:21 +0300

    +

--- ToDo List/Model.swift
+++ ToDo List/Model.swift
@@ -1,2 +1,21 @@
 import Foundation
 
+var toDoItems: [String] = ["Позвонить маме", "Написать приложение"]
+
+//Добавление
+func addItem(nameItem: String) {
+    toDoItems.append(nameItem)
+    saveData()
+}
+
+//Удаление
+func removeItem(at index: Int) {
+    toDoItems.remove(at: index)
+    saveData()
+}
+
+//Сохранение данных
+func saveData() {}
+
+//Загрузка данных
+func loadData() {}
--- ToDo List/TableTableViewController.swift
+++ ToDo List/TableTableViewController.swift
@@ -4,57 +4,57 @@ import UIKit
 
 class TableTableViewController: UITableViewController {
 
+    
+    @IBAction func pushAddAction(_ sender: Any) {
+        addItem(nameItem: "New Item")
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
+    //количество секций
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
+    //количество строк в секции
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return toDoItems.count
     }
 
-    /*
+   // генерация ячеек
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        //вывод в ячеку текста из массива
+        cell.textLabel?.text = toDoItems[indexPath.row]
 
         return cell
     }
-    */
-
-    /*
+    
+    //Редактирование ячейки по индексу
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            removeItem(at: indexPath.row)
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
